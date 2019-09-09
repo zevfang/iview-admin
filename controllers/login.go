55db2c983b5359e6ce512d38fdb178e30c1cd989
zevfang <fangwei0505@gmail.com>
2019-09-10 00:47:42 +0800

    增加terraformer库

--- controllers/login.go
+++ controllers/login.go
@@ -6,7 +6,7 @@ type LoginController struct {
 	beego.Controller
 }
 
-func (c *LoginController) Get() {
+func (c *LoginController) Index() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "login.html"
